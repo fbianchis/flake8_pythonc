a8c885a0c1f4269ed31c850c5b579019e7473cc5
Felipe Bianchi da Silva <fbianchidasilva@gmail.com>
2021-10-02 17:22:17 -0300

    add fila_base

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -23,7 +23,8 @@ class FilaPrioriaria(FilaBase):
             estatistica['dia'] = dia
             estatistica['agencia'] = agencia
             estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = \
+            estatistica['quantidade_clientes_atendidos'] = (
                 len(self.clientes_atendidos)
+            )
 
         return estatistica
